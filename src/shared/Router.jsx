e5a003b46978b91ca3bfdc44fdd70cecfcd8379e
Eunhye Jeong <gracejelly125@gmail.com>
2024-11-28 15:44:57 +0900

    Route Outlet 활용해서 접근 관리

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Profile from "../pages/Profile";
@@ -9,17 +15,16 @@ import Results from "../pages/Results";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
-
 // 로그인 상태일 때 해당 페이지 접근 가능, 아니면 login 페이지로 강제 이동
-const PrivateRoute = ({ element }) => {
+const PrivateRoute = () => {
   const { isAuthenticated } = useContext(AuthContext);
-  return isAuthenticated ? element : <Navigate to="/login" />;
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
 // 로그인 상태가 아닐 때, 해당 페이지(login) 접근 가능, 로그인 상태면 profile 페이지로 이동
-const PublicRoute = ({ element }) => {
+const PublicRoute = () => {
   const { isAuthenticated } = useContext(AuthContext);
-  return !isAuthenticated ? element : <Navigate to="/profile" />;
+  return !isAuthenticated ? <Outlet /> : <Navigate to="/profile" />;
 };
 
 const Router = () => {
@@ -28,14 +33,15 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="login" element={<PublicRoute element={<Login />} />} />
-          <Route path="signup" element={<PublicRoute element={<Signup />} />} />
-          <Route path="profile" element={<PrivateRoute element={<Profile />} />} />
-          <Route path="test" element={<PrivateRoute element={<TestPage />} />} />
-          <Route
-            path="results"
-            element={<PrivateRoute element={<Results />} />}
-          />
+          <Route element={<PublicRoute />}>
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+          </Route>
+          <Route element={<PrivateRoute />}>
+            <Route path="profile" element={<Profile />} />
+            <Route path="test" element={<TestPage />} />
+            <Route path="results" element={<Results />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
